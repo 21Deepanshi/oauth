86e24c3de7fd5ea901bfaac26fc2e4677d6f6162
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2025-06-05 17:43:41 -0600

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 // src/App.js
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ConnectSocials from './pages/instgram';
 
 function Home() {
-  const handleLogin = () => {
-    window.location.href = 'http://localhost:5000/auth/google';
-  };
-
     const handleInstagramConnect = () => {
     window.location.href = '/instagram';
   };
@@ -15,29 +11,9 @@ function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: 100 }}>
       <h1>OAuth Demo</h1>
-      <button onClick={handleLogin}>
-        Login with Google
-      </button>
       <button onClick={handleInstagramConnect}>
         Connect Instagram
       </button>
-    </div>
-  );
-}
-
-function OAuthSuccess() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const code = params.get('code');
-    console.log("Received code from Google:", code);
-  }, [location]);
-
-  return (
-    <div style={{ textAlign: 'center', marginTop: 100 }}>
-      <h1>Google Login Successful</h1>
-      <p>Welcome 😊</p>
     </div>
   );
 }
@@ -47,7 +23,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/oauth-success" element={<OAuthSuccess />} />
         <Route path='/instagram' element={<ConnectSocials />} /> 
       </Routes>
     </Router>
