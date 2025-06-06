967a81ca575b8858d54b3f335b94ce5a300008ca
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2025-06-05 19:12:57 -0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,6 @@ const instagramRoute = require('./routes/instagramRoutes');
 const app = express();
 app.use(cors());
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
-app.get('/auth/google', (req, res) => {
-  const authUrl =
-    'https://accounts.google.com/o/oauth2/v2/auth?' +
-    new URLSearchParams({
-      client_id: CLIENT_ID,
-      redirect_uri: REDIRECT_URI,
-      response_type: 'code',
-      scope: 'profile email'
-    }).toString();
-  res.redirect(authUrl);
-});
-
-app.get('/auth/google/callback', (req, res) => {
-  const code = req.query.code;
-  res.redirect(`https://oauth-1-l0jt.onrender.com/oauth-success?code=${code}`);
-});
 app.use('/api/instagram', instagramRoute);;
 app.listen(5000, () => console.log('OAuth server on http://localhost:5000'));
 
